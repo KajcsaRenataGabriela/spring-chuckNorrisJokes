ca464c667b40ccc473fab460dc80ead544cf7b83
KajcsaRenataGabriela <67558003+KajcsaRenataGabriela@users.noreply.github.com>
2021-09-20 12:52:54 +0300

    Add files via upload

---
+++ src/main/java/guru/springframework/chucknorrisquotes/controllers/JokeController.java
@@ -0,0 +1,24 @@
+package guru.springframework.chucknorrisquotes.controllers;
+
+import guru.springframework.chucknorrisquotes.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    //Spring will automatically inject this, no need for @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
---
+++ src/main/java/guru/springframework/chucknorrisquotes/services/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package guru.springframework.chucknorrisquotes.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl() {
+        //not friendly for Spring - this is hardcoding
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
